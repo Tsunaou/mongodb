1d3a701b1d305d3a05f26bc9d6cfd7ecfb83296f
young <895254752@qq.com>
2020-08-10 12:20:00 +0800

    update run.sh

---
+++ run_night.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+## w1 local
+lein run test -t causal-register --time-limit 180 -w w1 -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 5 --leave-db-running
+lein run test -t causal-register --time-limit 180 -w w1 -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 10 --leave-db-running
+lein run test -t causal-register --time-limit 180 -w w1 -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 20 --leave-db-running
+lein run test -t causal-register --time-limit 180 -w w1 -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 25 --leave-db-running
+lein run test -t causal-register --time-limit 180 -w w1 -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 50 --leave-db-running
+## w1 majority
+#lein run test -t causal-register --time-limit 180 -w w1 -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 5 --leave-db-running
+#lein run test -t causal-register --time-limit 180 -w w1 -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 10 --leave-db-running
+#lein run test -t causal-register --time-limit 180 -w w1 -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 20 --leave-db-running
+#lein run test -t causal-register --time-limit 180 -w w1 -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 25 --leave-db-running
+#lein run test -t causal-register --time-limit 180 -w w1 -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 50 --leave-db-running
+## majority local
+#lein run test -t causal-register --time-limit 180 -w majority -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 5 --leave-db-running
+#lein run test -t causal-register --time-limit 180 -w majority -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 10 --leave-db-running
+#lein run test -t causal-register --time-limit 180 -w majority -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 20 --leave-db-running
+#lein run test -t causal-register --time-limit 180 -w majority -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 25 --leave-db-running
+#lein run test -t causal-register --time-limit 180 -w majority -r local --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 50 --leave-db-running
+## majority majority
+lein run test -t causal-register --time-limit 180 -w majority -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 5 --leave-db-running
+lein run test -t causal-register --time-limit 180 -w majority -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 10 --leave-db-running
+lein run test -t causal-register --time-limit 180 -w majority -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 20 --leave-db-running
+lein run test -t causal-register --time-limit 180 -w majority -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 25 --leave-db-running
+lein run test -t causal-register --time-limit 180 -w majority -r majority --shard-count 2  --nodes-file /home/young/nodes  --password XR.123456 --concurrency 100 --leave-db-running --write-counts 100 --read-counts 200 --clients-per-key 50 --leave-db-running
