ca6a011adc67a4ef89d739bb0cfb7d3f5098efbf
young <895254752@qq.com>
2020-09-02 23:07:09 +0800

    Test ycsb-java

---
+++ src/jepsen/mongodb/YCSBKeyValue.java
@@ -0,0 +1,23 @@
+package jepsen.mongodb;
+
+public class YCSBKeyValue {
+    Integer key;
+    Integer value;
+
+    public YCSBKeyValue(Integer key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public static void main(String[] args) {
+        YCSBKeyValue kv = new YCSBKeyValue(1, null);
+    }
+}
