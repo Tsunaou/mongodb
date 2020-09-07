738d6d0edf4f3f307ff1338aebde6687160e7119
young <895254752@qq.com>
2020-09-07 16:26:20 +0800

    Update data0907.zip

--- run_update.sh
+++ run_update.sh
@@ -1,2 +1,21 @@
 #! /bin/bash
-lein run test -t causal-register --time-limit 600 -w w1 -r local --shard-count 2 --nodes-file /home/young/nodes  -ne true --password XR.123456 --concurrency 100 --operation-counts 20000 --read-proportion 0.5 --write-proportion 0.5 --leave-db-running
+# without nemesis
+# w1 local
+lein run test -t causal-register --time-limit 600 -w w1 -r local --shard-count 2 --nodes-file /home/young/nodes --with-nemesis false --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.5 --write-proportion 0.5 --leave-db-running
+lein run test -t causal-register --time-limit 600 -w w1 -r local --shard-count 2 --nodes-file /home/young/nodes --with-nemesis false --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.2 --write-proportion 0.8 --leave-db-running
+lein run test -t causal-register --time-limit 600 -w w1 -r local --shard-count 2 --nodes-file /home/young/nodes --with-nemesis false --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.8 --write-proportion 0.2 --leave-db-running
+# majority majority
+lein run test -t causal-register --time-limit 600 -w majority -r majority --shard-count 2 --nodes-file /home/young/nodes --with-nemesis false --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.5 --write-proportion 0.5 --leave-db-running
+lein run test -t causal-register --time-limit 600 -w majority -r majority --shard-count 2 --nodes-file /home/young/nodes --with-nemesis false --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.2 --write-proportion 0.8 --leave-db-running
+lein run test -t causal-register --time-limit 600 -w majority -r majority --shard-count 2 --nodes-file /home/young/nodes --with-nemesis false --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.8 --write-proportion 0.2 --leave-db-running
+# with nemesis
+# w1 local
+lein run test -t causal-register --time-limit 600 -w w1 -r local --shard-count 2 --nodes-file /home/young/nodes --with-nemesis true --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.5 --write-proportion 0.5 --leave-db-running
+lein run test -t causal-register --time-limit 600 -w w1 -r local --shard-count 2 --nodes-file /home/young/nodes --with-nemesis true --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.2 --write-proportion 0.8 --leave-db-running
+lein run test -t causal-register --time-limit 600 -w w1 -r local --shard-count 2 --nodes-file /home/young/nodes --with-nemesis true --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.8 --write-proportion 0.2 --leave-db-running
+# majority majority
+lein run test -t causal-register --time-limit 600 -w majority -r majority --shard-count 2 --nodes-file /home/young/nodes --with-nemesis true --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.5 --write-proportion 0.5 --leave-db-running
+lein run test -t causal-register --time-limit 600 -w majority -r majority --shard-count 2 --nodes-file /home/young/nodes --with-nemesis true --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.2 --write-proportion 0.8 --leave-db-running
+lein run test -t causal-register --time-limit 600 -w majority -r majority --shard-count 2 --nodes-file /home/young/nodes --with-nemesis true --password XR.123456 --concurrency 10 --operation-counts 20000 --read-proportion 0.8 --write-proportion 0.2 --leave-db-running
+
+
